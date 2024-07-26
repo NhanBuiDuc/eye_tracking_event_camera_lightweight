803198a7031b88fe0f0d03e44ee12b9d9f600986
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2024-07-26 22:25:18 +0800

    fix

--- data/dataset/DynamicDataset.py
+++ data/dataset/DynamicDataset.py
@@ -18,22 +18,22 @@ class DynamicDatasetHz10000(DatasetHz10000):
         self.data_queue = queue.Queue()
         self.load_data_into_cache()
         self.load_next_data_thread()
-        
+
     def load_data_into_cache(self):
-        for i in range(min(self.cache_size, len(self.data_paths))):
+        for i in range(min(self.cache_size, len(self.data_idx))):
             self._load_data(i)
 
     def _load_data(self, idx):
-        data_file = self.data_paths[idx]
+        data_file = os.path.join(self.cache_data_dir, f'{self.data_idx[idx]}_data.h5')
         with h5py.File(data_file, 'r') as hf:
             self.data_cache[idx] = hf['data'][:]
             self.labels_cache[idx] = hf['labels'][:]
 
     def load_next_data_thread(self):
         def load_next():
-            for idx in range(self.cache_size, len(self.data_paths)):
+            for idx in range(self.cache_size, len(self.data_idx)):
                 self.data_queue.put(idx)
-        
+
         thread = Thread(target=load_next)
         thread.start()
 
@@ -46,8 +46,9 @@ class DynamicDatasetHz10000(DatasetHz10000):
         return sum(len(data) for data in self.data_cache.values())
 
     def __getitem__(self, index):
-        cache_idx = index // len(self.data_cache[0])
-        relative_idx = index % len(self.data_cache[0])
+        # Calculate which cache and relative index to fetch
+        cache_idx = index // len(next(iter(self.data_cache.values())))
+        relative_idx = index % len(next(iter(self.data_cache.values())))
 
         if cache_idx not in self.data_cache:
             with self.lock:
@@ -56,7 +57,7 @@ class DynamicDatasetHz10000(DatasetHz10000):
                     self._load_data(next_data_idx)
                     if len(self.data_cache) > self.cache_size:
                         self.unload_old_data(cache_idx - self.cache_size)
-        
+
         data = self.data_cache[cache_idx][relative_idx]
         label = self.labels_cache[cache_idx][relative_idx]
         return torch.tensor(data, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)

520c814b8aa4c5bf774624064f4c54936c4ba95d
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2024-08-13 01:36:57 +0800

    remove sigmoid

--- model/simple_convlstm3.py
+++ model/simple_convlstm3.py
@@ -385,7 +385,7 @@ class SimpleConvLSTM3(nn.Module):
         sy = sy /  self.stimulus_screen_size[0]
 
         coordinate_pred = torch.stack((sx, sy), dim=1)
-        coordinate_pred = F.sigmoid(coordinate_pred)
+
         # 2. Apply softmax to the last four channels
         state_pred = data[:, 2:]  # Shape: (batch_size, 4, h, w)
         state_pred = F.softmax(state_pred, dim=1)  # Apply softmax on the class dimension
